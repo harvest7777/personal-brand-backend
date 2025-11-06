9db7ce2abec187fd1c38a20679065f8416e375b1
harvest7777 <333harvest.moon333@gmail.com>
2025-11-06 14:23:49 -0800

    feat: helper to list what to delete

--- langgraph_logic/delete_agent/delete_helpers.py
+++ langgraph_logic/delete_agent/delete_helpers.py
@@ -1,5 +1,6 @@
 from langchain_core.messages import HumanMessage
 from langgraph_logic.shared_clients.llm_client import shared_llm
+from chroma.shared_chroma_client import collection
 
 def is_valid_delete_request(user_input: str) -> bool:
     """Check if the user's message is a valid delete request"""
@@ -35,7 +36,16 @@ def is_affirmative_response(user_input: str) -> bool:
     answer = response.content.strip().lower()  # type: ignore
     return answer == "yes"
 
+def to_delete_from_user_input(user_input: str) -> list[str]:
+    """Extract the data to delete from the user's input"""
+    documents = []
+    result = collection.query(query_texts=user_input, n_results=5)
+    for i in range(len(result["documents"])): # type: ignore
+        if result["distances"][i][0] <= 2: # type: ignore
+            documents.append(result["documents"][i][0]) # type: ignore
+
+    return documents
+
 if __name__ == "__main__":
-    print(is_valid_delete_request("I want to delete my data"))
-    print(is_valid_delete_request("I want to delete my data about my work experience at Walmart"))
-    print(is_valid_delete_request("i wanna delete datas about my high school"))
+    docs = to_delete_from_user_input("college")
+    print(docs)
